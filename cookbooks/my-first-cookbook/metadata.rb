1d64c15d70bc328bd8187ab941051bcbf09fc640
florian <florian.drescher@gmail.com>
2011-09-28 01:40:41 -0700

    hello.

--- cookbooks/my-first-cookbook/metadata.rb
+++ cookbooks/my-first-cookbook/metadata.rb
@@ -5,4 +5,11 @@ description      "Installs/Configures my-first-cookbook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 recipe "my-first-cookbook::helloworld", "My first recipe, prints Hello World to the RightScale dashboard"
+recipe "my-first-cookbook::hello", "My second recipe, prints Hello NAME to the RightScale dashboard"
+
+attribute "name",
+:display_name => "Test value",
+:description => "Test specifying Input via Chef attribute",
+:required => true,
+:recipes => [ "my-first-cookbook::hello" ]
 
